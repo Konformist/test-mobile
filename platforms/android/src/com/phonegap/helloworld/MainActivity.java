1b5541ccc5ff592a2e314a5b1544f58a748c0484
Konstantin Jakovlev <konstantinya.dev@websm.io>
2018-02-12 16:35:41 +0300

    test

--- platforms/android/src/com/phonegap/helloworld/MainActivity.java
+++ platforms/android/src/com/phonegap/helloworld/MainActivity.java
@@ -17,44 +17,25 @@
        under the License.
  */
 
-// package com.phonegap.helloworld;
-
-// import android.os.Bundle;
-// import org.apache.cordova.*;
-
-// public class MainActivity extends CordovaActivity
-// {
-//     @Override
-//     public void onCreate(Bundle savedInstanceState)
-//     {
-//         super.onCreate(savedInstanceState);
-
-//         // enable Cordova apps to be started in the background
-//         Bundle extras = getIntent().getExtras();
-//         if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
-//             moveTaskToBack(true);
-//         }
-
-//         // Set by <content src="index.html" /> in config.xml
-//         loadUrl(launchUrl);
-//     }
-// }
-
 package com.phonegap.helloworld;
+
 import android.os.Bundle;
 import org.apache.cordova.*;
-import android.webkit.WebView;
-import android.webkit.WebSettings;
 
-public class MainActivity extends CordovaActivity { 
-    @Override public void onCreate(Bundle savedInstanceState) { 
-
+public class MainActivity extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
-        WebView webView = (WebView) appView.getEngine().getView();
-        WebSettings settings = webView.getSettings();
-        settings.setBuiltInZoomControls(true);
-        settings.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM); 
-        settings.setSupportZoom(true);
+        // enable Cordova apps to be started in the background
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+            moveTaskToBack(true);
+        }
+
+        // Set by <content src="index.html" /> in config.xml
+        loadUrl(launchUrl);
     }
 }
